de23f382cc5008a8435a0b9cc81a73e4402ecc8b
教育1 码农 <manongjiaoyu@manongjiaoyu1demac-mini.local>
2016-05-09 14:43:58 +0800

    从前有个小胖子   名字叫做睢意博

--- Next/ViewController.m
+++ Next/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"睢意博你个小胖子");
 }
 
 - (void)didReceiveMemoryWarning {
